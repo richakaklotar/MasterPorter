1de2d906a301bcacdbb7dcc47520368aaa61dd5b
Richa Kaklotar <richakaklotar.mscit19@vnagu.ac.in>
2026-08-25 15:04:54 +0530

    cors change

--- MasterPorter/Controllers/ActivitiesController.cs
+++ MasterPorter/Controllers/ActivitiesController.cs
@@ -1,18 +1,16 @@
-﻿using AutoEntity.EntityModels;
-using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AutoEntity.EntityModels;
 
 namespace MasterPorter.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [EnableCors("AllowReactApp")]
     public class ActivitiesController : ControllerBase
     {
         private readonly MasterPorterContext _context;
